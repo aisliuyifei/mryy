148f72653799f076245cb2ceeaea82ba2f05af64
wupeng <wupeng@LeideMac-Pro.local>
2015-07-10 16:16:22 +0800

    a

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
     end
   end
   
-  get 'ls' =>"ls#index"
-  
+  get 'ls' =>"ls#index"  
   root "mottos#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
